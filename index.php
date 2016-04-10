95c4b21b11f6b35a19a00ccf717b4ce61e46beb6
Kees Romkes <kees@styla.com>
2016-04-10 19:35:34 +0200

    Added different way of displaying items on mobile

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ get_header(); ?>
 	<!-- this is a comment that v2.1 exists -->
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
@@ -32,53 +32,77 @@ get_header(); ?>
 			<?php $i++; ?>
 			<?php endwhile; ?>
 			<?php rewind_posts(); ?>
-			<div class="row">
-				
-					<?php $i = 1; ?>
-					<div class="col-xs-12 col-md-6 smallarticle">
-					<?php while (have_posts()) : the_post(); ?>
-						
-						<?php if (($i >= 1) && ($i % 2 == 0)) { ?>
-						
+			<div class="biggerthanmobile">
+				<div class="row">
+					
+						<?php $i = 1; ?>
+						<div class="col-md-6 smallarticle">
+						<?php while (have_posts()) : the_post(); ?>
 							
-								<?php
-									/* Include the Post-Format-specific template for the content.
-									 * If you want to override this in a child theme, then include a file
-									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-									 */
-									get_template_part( 'content-small' );
-								?>
+							<?php if (($i >= 1) && ($i % 2 == 0)) { ?>
 							
-						<?php } 
-						
-						?>
-						
-					<?php $i++; ?>
-					<?php endwhile; ?>
-					</div>
-					<?php rewind_posts(); ?>
-					<?php $i = 1; ?>
-					<div class="col-xs-12 col-md-6 smallarticle">
-					<?php while (have_posts()) : the_post(); ?>
-						
-						
-							<?php if (($i > 1) && ($i % 2 == 1)) { ?>
-								<?php
-									/* Include the Post-Format-specific template for the content.
-									 * If you want to override this in a child theme, then include a file
-									 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-									 */
-									get_template_part( 'content-small' );
-								?>
+								
+									<?php
+										/* Include the Post-Format-specific template for the content.
+										 * If you want to override this in a child theme, then include a file
+										 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+										 */
+										get_template_part( 'content-small' );
+									?>
+								
+							<?php } 
 							
-						<?php } 
+							?>
+							
+						<?php $i++; ?>
+						<?php endwhile; ?>
+						</div>
+						<?php rewind_posts(); ?>
+						<?php $i = 1; ?>
+						<div class="col-md-6 smallarticle">
+						<?php while (have_posts()) : the_post(); ?>
+							
+							
+								<?php if (($i > 1) && ($i % 2 == 1)) { ?>
+									<?php
+										/* Include the Post-Format-specific template for the content.
+										 * If you want to override this in a child theme, then include a file
+										 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+										 */
+										get_template_part( 'content-small' );
+									?>
+								
+							<?php } 
+							
+							?>
+							
+						<?php $i++; ?>
+						<?php endwhile; ?>
+						</div>
+					
+				</div>
+			</div>	
+			<?php rewind_posts(); ?>
+			<div class="mobile">
+				<div class="row">
+					
+						<div class="col-xs-12 smallarticle">
+						<?php while (have_posts()) : the_post(); ?>
+							
+								
+									<?php
+										/* Include the Post-Format-specific template for the content.
+										 * If you want to override this in a child theme, then include a file
+										 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+										 */
+										get_template_part( 'content-small' );
+									?>
+							
+						<?php endwhile; ?>
+						</div>
 						
-						?>
-						
-					<?php $i++; ?>
-					<?php endwhile; ?>
-					</div>
-				
+					
+				</div>
 			</div>
 			<?php sparkling_paging_nav(); ?>
 
