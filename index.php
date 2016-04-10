22a0e71633bbfe75250d8dc830571da886c3e673
Kees Romkes <kees@styla.com>
2016-04-10 20:02:38 +0200

    Added header to be only part of non mobile

--- index.php
+++ index.php
@@ -17,22 +17,22 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php $i = 1; ?>
-			<?php while (have_posts() && $i < 2) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-			<?php $i++; ?>
-			<?php endwhile; ?>
-			<?php rewind_posts(); ?>
+	
 			<div class="biggerthanmobile">
+				<?php /* Start the Loop */ ?>
+				<?php $i = 1; ?>
+				<?php while (have_posts() && $i < 2) : the_post(); ?>
+
+					<?php
+						/* Include the Post-Format-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+						 */
+						get_template_part( 'content', get_post_format() );
+					?>
+				<?php $i++; ?>
+				<?php endwhile; ?>
+				<?php rewind_posts(); ?>
 				<div class="row">
 					
 						<?php $i = 1; ?>
